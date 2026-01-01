c6d820259da7e95d3c3d3fe4bc5a41902ab7e7e6
Steph DGenius <stephanetse@gmail.com>
2026-01-01 20:33:55 +0100

    💄 Update default pinned apps in moment dock

--- components/momentDock/Constants.ts
+++ components/momentDock/Constants.ts
@@ -1,13 +1,14 @@
-import { AppId } from '../../types';
-import { APP_REGISTRY } from '../../registry/AppRegistry';
+import { AppId } from "../../types";
+import { APP_REGISTRY } from "../../registry/AppRegistry";
 
 // Re-exporting metadata structure from registry for the sidebar components
 export const APP_METADATA = APP_REGISTRY;
 
 export const PINNED_APPS: AppId[] = [
-  AppId.PAPER_WRITER,
-  AppId.WEB_TV,
-  AppId.SHOPPER,
-  AppId.WEB_STUDIO,
-  AppId.ANALYTICS
+  AppId.CALL,
+  AppId.MESSAGES,
+  AppId.FILES,
+  AppId.MUSIC,
+  AppId.VIDEO_PLAYER,
+  AppId.PAPER_NOTES,
 ];
